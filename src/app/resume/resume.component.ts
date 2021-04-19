ffa56002a8ac38d5a1c28d9b75f187b4ed9c7b2e
rachisalive <rachelodumu@gmail.com>
2021-04-19 00:26:16 -0400

    Update resume + add favicon

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResumeComponent implements OnInit
 {
-  pdf: string = './assets/pdfs/Rachel_Odumu_2020_Resume.pdf';
+  pdf: string = './assets/pdfs/rachel_odumu_2021_resume.pdf';
   constructor() { }
 
   ngOnInit(): void
