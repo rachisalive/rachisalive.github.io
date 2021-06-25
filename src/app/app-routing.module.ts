a3d87c7faaa9d4755de63029cd1bdf5a6bdd78b3
Rachel Odumu <rachelodumu@gmail.com>
2021-06-25 00:40:54 -0400

    Change theme + add vinstripe work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { ResumeComponent } from './resume/resume.component';
 const routes: Routes =
   [
     { path: '', loadChildren: () => import('./portfolio/portfolio.module').then(m => m.PortfolioModule) },
+    { path: 'work', loadChildren: () => import('./work/work.module').then(m => m.WorkModule) },
     { path: 'resume', component: ResumeComponent }
   ];
 
---
+++ src/app/work/work-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { WorkComponent } from './work.component';
+
+const routes: Routes =
+  [
+    { path: '', component: WorkComponent },
+    // { path: 'vinstripe'}
+  ];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class WorkRoutingModule { }
